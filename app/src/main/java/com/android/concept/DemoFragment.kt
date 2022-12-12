9ef5144df25dd0aa27ffc4a5aeb05eaff474d57d
Nitesh Dewada <nitesh.dewada@go-upwards.com>
2022-12-12 16:51:39 +0530

    Changes Done

---
+++ app/src/main/java/com/android/concept/DemoFragment.kt
@@ -0,0 +1,84 @@
+package com.android.concept
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class DemoFragment : Fragment() {
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        println("Fragment : onAttach")
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        println("Fragment : onCreate")
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        println("Fragment : onCreateView")
+
+        return inflater.inflate(R.layout.fragment_demo, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        println("Fragment : onActivityCreated")
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("Fragment : onStart")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("Fragment : onResume")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("Fragment : onPause")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("Fragment : onStop")
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        println("Fragment : onDestroyView")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("Fragment : onDestroy")
+
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        println("Fragment : onDetach")
+
+    }
+
+}
--- app/src/main/java/com/android/concept/MainActivity.kt
+++ app/src/main/java/com/android/concept/MainActivity.kt
@@ -2,10 +2,54 @@ package com.android.concept
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println("Activity : onCreate")
+
+        addFragment(DemoFragment())
+    }
+
+    private fun addFragment(fragment: Fragment) {
+
+        var fragmentManager = supportFragmentManager
+        var fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
+        println("Activity : addFragment")
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("Activity : onStart")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("Activity : onResume")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("Activity : onPause")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("Activity : onStop")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("Activity : onDestroy")
+
     }
 }
