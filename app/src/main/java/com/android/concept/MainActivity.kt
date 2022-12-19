30de962799eedd415e7b3464420a77e97a6041f0
Nitesh Dewada <nitesh.dewada@go-upwards.com>
2022-12-19 16:36:03 +0530

    Changes Done

--- app/src/main/java/com/android/concept/MainActivity.kt
+++ app/src/main/java/com/android/concept/MainActivity.kt
@@ -2,28 +2,13 @@ package com.android.concept
 
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.widget.TableLayout
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
-import androidx.fragment.app.Fragment
-
-import androidx.fragment.app.FragmentOnAttachListener
 import androidx.fragment.app.FragmentTransaction
-import androidx.navigation.Navigation
-import com.android.concept.adapter.ViewpagerAdapter
 import com.android.concept.databinding.ActivityMainBinding
-import com.android.concept.utils.Utils
-import com.google.android.material.navigation.NavigationView
-import com.google.android.material.tabs.TabLayout
 
 
-class MainActivity : AppCompatActivity(),TabLayout.OnTabSelectedListener{
+class MainActivity : AppCompatActivity(){
 
-    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
     private lateinit var binding: ActivityMainBinding
     var fragmentManager = supportFragmentManager
     private lateinit var fragmentTransaction : FragmentTransaction
@@ -37,24 +22,8 @@ class MainActivity : AppCompatActivity(),TabLayout.OnTabSelectedListener{
 
         supportActionBar?.title = "Android Demo"
 
-        setupTabLayout()
     }
 
-    private fun setupTabLayout() {
-
-        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Home"))
-        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Profile"))
-        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Setting"))
-
-        val adapter = ViewpagerAdapter(supportFragmentManager,binding.tabLayout.tabCount)
-
-        binding.viewPager.adapter = adapter
-
-        binding.viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(binding.tabLayout))
-
-        binding.tabLayout.addOnTabSelectedListener(this)
-
-    }
 
 
     override fun onStart() {
@@ -93,19 +62,6 @@ class MainActivity : AppCompatActivity(),TabLayout.OnTabSelectedListener{
 
     }
 
-    override fun onTabSelected(tab: TabLayout.Tab?) {
-        if (tab != null) {
-            binding.viewPager.currentItem = tab.position
-        }
-    }
-
-    override fun onTabUnselected(tab: TabLayout.Tab?) {
-
-    }
-
-    override fun onTabReselected(tab: TabLayout.Tab?) {
-
-    }
 
 
 }
