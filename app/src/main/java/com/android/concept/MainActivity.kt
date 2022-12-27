e94e0a86b329114b10d800936584bf8d2fcbc36b
Nitesh Dewada <nitesh.dewada@go-upwards.com>
2022-12-27 14:40:11 +0530

    Changes Done

--- app/src/main/java/com/android/concept/MainActivity.kt
+++ app/src/main/java/com/android/concept/MainActivity.kt
@@ -1,18 +1,46 @@
 package com.android.concept
 
 
+import android.Manifest
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.view.View
-import android.widget.Button
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentTransaction
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.android.concept.databinding.ActivityMainBinding
 import com.android.concept.utils.Utils
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.DexterError
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
+
+
+    private val TAG = "PermissionDemo"
+    private val REQUEST_CODE = 101
+
+
+    private lateinit var builder: Notification.Builder
+    private lateinit var notificationChannel: NotificationChannel
+    private lateinit var notificationManager: NotificationManager
 
     private lateinit var binding: ActivityMainBinding
 
@@ -25,46 +53,174 @@ class MainActivity : AppCompatActivity(){
 
         supportActionBar?.title = "Android Demo"
 
-
+        setupClickListners()
     }
 
 
-    override fun onStart() {
-        super.onStart()
-        println("Activity : onStart")
+    private fun setupClickListners() {
+        binding.buttonRequestPermission.setOnClickListener(View.OnClickListener {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+                //askPermission()
+                requestPermissions()
+            }
+        })
 
-    }
-
-    override fun onResume() {
-        super.onResume()
-        println("Activity : onResume")
-
-    }
-
-    override fun onPause() {
-        super.onPause()
-        println("Activity : onPause")
-
-    }
-
-    override fun onStop() {
-        super.onStop()
-        println("Activity : onStop")
-
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        println("Activity : onDestroy")
-
-    }
-
-    override fun onBackPressed() {
-
-        super.onBackPressed()
-
+        binding.buttonSendNotification.setOnClickListener(View.OnClickListener {
+            addNotification()
+        })
     }
 
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun requestPermissions() {
+        // below line is use to request permission in the current activity.
+        // this method is use to handle error in runtime permissions
+        Dexter.withActivity(this) // below line is use to request the number of permissions which are required in our app.
+            .withPermissions(
+                Manifest.permission.CAMERA,  // below is the list of permissions
+                Manifest.permission.POST_NOTIFICATIONS,
+            ) // after adding permissions we are calling an with listener method.
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(multiplePermissionsReport: MultiplePermissionsReport) {
+                    // this method is called when all permissions are granted
+                    if (multiplePermissionsReport.areAllPermissionsGranted()) {
+                        // do you work now
+                        Toast.makeText(
+                            this@MainActivity,
+                            "All the permissions are granted..",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    // check for permanent denial of any permission
+                    if (multiplePermissionsReport.isAnyPermissionPermanentlyDenied) {
+                        // permission is denied permanently, we will show user a dialog message.
+                        showSettingsDialog()
+                    }
+                }
 
+                override fun onPermissionRationaleShouldBeShown(
+                    list: List<PermissionRequest?>?,
+                    permissionToken: PermissionToken
+                ) {
+                    // this method is called when user grants some permission and denies some of them.
+                    permissionToken.continuePermissionRequest()
+                }
+            }).withErrorListener { error: DexterError? ->
+                // we are displaying a toast message for error message.
+                Toast.makeText(applicationContext, "Error occurred! ", Toast.LENGTH_SHORT).show()
+            } // below line is use to run the permissions on same thread and to check the permissions
+            .onSameThread().check()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CODE -> {
+
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+
+                    Utils.showToast("Permission Not Granted!")
+
+                } else {
+                    Utils.showToast("Permission Granted!")
+                }
+            }
+        }
+    }
+
+    private fun addNotification() {
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            notificationChannel =
+                NotificationChannel("123", "this is test Notification", NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(notificationChannel)
+            builder =
+                Notification.Builder(this, "123").setContentTitle("Notification title")
+                    .setContentText("Test Notification")
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setContentIntent(pendingIntent)
+        }
+        notificationManager.notify(12345, builder.build())
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun askPermission() {
+        val permission = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.POST_NOTIFICATIONS
+        )
+
+        if(permission == PackageManager.PERMISSION_GRANTED){
+            Utils.showToast("Permission Already Given")
+        }else{
+            if(ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.POST_NOTIFICATIONS)
+            ){
+                showDialogForPermissionExplaination()
+            }else{
+                if(Utils.isPermissionAskedFirstTime){
+                    makeRequest()
+                    Utils.isPermissionAskedFirstTime = false
+                }else{
+                    showSettingsDialog()
+                }
+            }
+        }
+    }
+
+    private fun showSettingsDialog() {
+        val builder = AlertDialog.Builder(this@MainActivity)
+
+        builder.setTitle("Need Permissions")
+
+        builder.setMessage("This app needs permission to use this feature. You can grant them in app settings.")
+        builder.setPositiveButton("GOTO SETTINGS") { dialog, which ->
+            dialog.cancel()
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+            val uri = Uri.fromParts("package", packageName, null)
+            intent.data = uri
+            startActivityForResult(intent, 101)
+        }
+        builder.setNegativeButton("Cancel") { dialog, which ->
+            dialog.cancel()
+        }
+        builder.show()
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun showDialogForPermissionExplaination() {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("Notification permission is required for this app to send notifications.")
+            .setTitle("Permission required")
+
+        builder.setPositiveButton(
+            "OK"
+        ) { dialog, id ->
+            Log.i(TAG, "Clicked")
+            makeRequest()
+        }
+
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun makeRequest() {
+
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+            REQUEST_CODE
+        )
+    }
 }
--- app/src/main/java/com/android/concept/utils/Utils.kt
+++ app/src/main/java/com/android/concept/utils/Utils.kt
@@ -1,9 +1,21 @@
 package com.android.concept.utils
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.widget.Toast
 import com.android.concept.MainApplication
 
 object Utils {
+
+    private var PREF_PERMISSION = "pref_permission"
+
+    private val preferences: SharedPreferences = MainApplication.appContext.getSharedPreferences("global_shared_preference",Context.MODE_PRIVATE)
+
+
+    var isPermissionAskedFirstTime: Boolean
+        get() = preferences.getBoolean(PREF_PERMISSION, true)
+        set(value) = preferences.edit().putBoolean(PREF_PERMISSION,value).apply()
+
     fun showToast(message : String){
         Toast.makeText(MainApplication.appContext,message,Toast.LENGTH_LONG).show()
     }
