931f716a56fdf797f334abeeaf8a797f1050738c
Nitesh Dewada <nitesh.dewada@go-upwards.com>
2023-01-04 17:39:07 +0530

    Changes Done

--- app/src/main/java/com/android/concept/MainActivity.kt
+++ app/src/main/java/com/android/concept/MainActivity.kt
@@ -37,10 +37,7 @@ import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var thread: Thread
     private lateinit var binding: ActivityMainBinding
-    var threadCounter = 0
-    var isThreadRunning = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,106 +50,7 @@ class MainActivity : AppCompatActivity() {
 
         Timber.d("Current Thread id : "+Thread.currentThread().id.toString())
 
-        //coroutineDemo()
-        //dispatchersDemo()
-//        runBlocking {
-//            startCounter()
-//        }
-
-        //asyncDemo()
-        setupClickListners()
     }
 
-    private fun asyncDemo() {
-        GlobalScope.launch {
-            val str = async{ receiveDataFromServer()}
-            Timber.d(str.await())
-        }
-    }
-
-    suspend fun receiveDataFromServer(): String{
-        delay(5000)
-        return "Data Fetched From Server"
-    }
-
-    private fun dispatchersDemo() {
-//        GlobalScope.launch(Dispatchers.Main) {
-//            startCounter()
-//        }
-
-//        GlobalScope.launch(Dispatchers.IO) {
-//            startCounter()
-//        }
-//
-//        GlobalScope.launch(Dispatchers.Default) {
-//            startCounter()
-//
-//        }
-
-        GlobalScope.launch(Dispatchers.Unconfined) {
-            startCounter()
-
-        }
-    }
-
-    private fun coroutineDemo() {
-
-//        GlobalScope.launch {
-//            startCounter()
-//        }
-
-        lifecycleScope.launch{
-           startCounter()
-        }
-    }
-
-    suspend fun startCounter(){
-        while (true){
-            //delay(1000)
-            Thread.sleep(1000)
-            threadCounter += 1
-            Timber.d("Current Thread id : "+Thread.currentThread().id.toString())
-            binding.textView.text = threadCounter.toString()
-        }
-    }
-    private fun setupClickListners() {
-
-        binding.buttonStartTestActivity.setOnClickListener{
-            startActivity(Intent(this,TestActivity::class.java))
-            finish()
-        }
-
-        var job : Job? = null
-        binding.buttonStart.setOnClickListener {
-            isThreadRunning = true
-
-            job = GlobalScope.launch { startCounter() }
-        }
-
-        binding.buttonStop.setOnClickListener{
-            isThreadRunning = false
-            job?.cancel()
-        }
-    }
-
-    inner class MyAsyncTask : AsyncTask<Int, Int, Unit>() {
-
-        override fun doInBackground(vararg params: Int?) {
-            var counter = params[0]
-            while (isThreadRunning){
-                SystemClock.sleep(1000)
-                counter = counter?.plus(1)
-                Timber.d("Current Thread id : "+Thread.currentThread().id.toString())
-                publishProgress(counter)
-            }
-        }
-
-        override fun onProgressUpdate(vararg values: Int?) {
-            super.onProgressUpdate(*values)
-
-            binding.textView.text = values[0].toString()
-        }
-
-    }
 
 }
